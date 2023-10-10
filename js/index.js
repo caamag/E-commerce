32e2aa1f697a9e5dc7c9fa03715245cd12fb3df3
Caio Lopes <caiolopesfv@gmail.com>
2023-10-10 20:52:21 -0300

    enviando dados

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+//nav mobile
+const navButton = document.querySelector('.menu-button'); 
+const navXButton = document.querySelector('.menu-X-button'); 
+const navMobile = document.querySelector('.items-mobile'); 
+
+navButton.addEventListener('click', () => {
+
+    navMobile.classList.toggle('nav-change'); 
+
+    setTimeout(() => {
+
+        navButton.style.display = 'none'; 
+        navXButton.style.display = 'block'; 
+
+    }, 400)
+
+})
+
+navXButton.addEventListener('click', () => {
+
+    navMobile.classList.toggle('nav-change'); 
+
+    setTimeout(() => {
+
+        navButton.style.display = 'block'; 
+        navXButton.style.display = 'none'; 
+
+    }, 400)
+
+})
+
+
