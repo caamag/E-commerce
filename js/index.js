4ebb442c1975dfef2ef2ba5e29e59c917b64ccf1
Caio Lopes <caiolopesfv@gmail.com>
2023-11-20 21:08:42 -0300

    enviando finalização

--- js/index.js
+++ js/index.js
@@ -31,4 +31,16 @@ navXButton.addEventListener('click', () => {
 }); 
 
 
-//slider
+//opening time
+const opening = document.querySelector('.opening'); 
+const data = new Date(); 
+const hour = data.getHours(); 
+const day = data.getDay(); 
+
+if ((hour >= 19 && day >= 1 && day < 7) || day === 7) {
+    opening.innerHTML = 'Fechado (segunda a sábado 10:00 às 19:00)'; 
+    opening.classList.toggle('closed')
+} else if (hour >= 10 && hour < 19 && day >= 1 && day < 7) {
+    opening.innerHTML = 'Aberto (segunda a sábado 10:00 às 19:00)'; 
+    opening.classList.toggle('open');   
+}
