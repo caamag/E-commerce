8e97eb15d4b5de50881b6b16ce6536fad26d3d58
BCR-0632 <caiolopesfv@gmail.com>
2023-11-28 16:29:06 -0300

    teste

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ const day = data.getDay();
 
 if ((hour >= 19 && day >= 1 && day < 7) || day === 7) {
     opening.innerHTML = 'Fechado (segunda a sábado 10:00 às 19:00)'; 
-    opening.classList.toggle('closed')
+    opening.classList.toggle('closed'); 
 } else if (hour >= 10 && hour < 19 && day >= 1 && day < 7) {
     opening.innerHTML = 'Aberto (segunda a sábado 10:00 às 19:00)'; 
     opening.classList.toggle('open');   
